7add397c3be43e167bbe760e9b7c1acae155a9ba
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-25 03:13:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstChar(text) {
-  return text.charAt(0);
+  return text.charAt(1);
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-// const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
+
